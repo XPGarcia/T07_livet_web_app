d55257e48736d85445297350d2d556633369534d
Xavier Garcia <xavicogarcia12@gmail.com>
2022-05-27 21:37:46 -0500

    React Scheduler dependency added

--- src/Pages/Schedule/Schedule.js
+++ src/Pages/Schedule/Schedule.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Calendar from '../../Components/Calendar/Calendar';
 import SidebarLayout from '../../Layouts/SidebarLayout';
 
 function Schedule(props) {
   return(
     <SidebarLayout>
-      <h2>Hola, soy el calendario de citas</h2>
+      <Calendar />
     </SidebarLayout>
   );
 }
