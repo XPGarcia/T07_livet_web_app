4d0e8cbc1377b11e8a5c1ec429b9860d4553b804
Xavier Garcia <xavicogarcia12@gmail.com>
2022-07-11 08:35:53 -0500

    package.json updated and new page added (DoctorScheduler)

---
+++ src/Pages/DoctorScheduler/DoctorScheduler.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SidebarLayout from "../../Layouts/SidebarLayout";
+
+function DoctorScheduler() {
+  return (
+    <SidebarLayout>
+      <h2>HOLA, soy el agendador de horarios de los doctores.</h2>
+    </SidebarLayout>
+  );
+}
+
+export default DoctorScheduler;
--- src/Routes/Secretary/routes.js
+++ src/Routes/Secretary/routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import EventIcon from "@mui/icons-material/Event";
 import PeopleIcon from "@mui/icons-material/People";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 
 const secretaryRoutes = [
   {
@@ -12,6 +13,11 @@ const secretaryRoutes = [
     name: "Pacientes",
     link: "/pacientes",
     icon: <PeopleIcon />
+  },
+  {
+    name: "Doctores",
+    link: "/doctores",
+    icon: <ManageAccountsIcon />
   }
 ];
 
