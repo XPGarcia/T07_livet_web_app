910db900fe1437491924cfe1dba4ef33b0d17a64
Xavier Garcia <xavicogarcia12@gmail.com>
2022-07-14 14:05:28 -0500

    Dockerfile and docker-compose.yml

--- src/Pages/DoctorScheduler/DoctorScheduler.js
+++ src/Pages/DoctorScheduler/DoctorScheduler.js
@@ -4,7 +4,7 @@ import SidebarLayout from "../../Layouts/SidebarLayout";
 function DoctorScheduler() {
   return (
     <SidebarLayout>
-      <h2>HOLA, soy el agendador de horarios de los doctores.</h2>
+      <h2>HOLA, soy el AGENDADOR de horarios de los doctores.</h2>
     </SidebarLayout>
   );
 }
